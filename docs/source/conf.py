49384e649ace793e654a39b407f3a5292518492a
陈泽 <chenze_ustc@mail.ustc.edu.cn>
2021-10-01 22:23:24 +0800

    Berry's phase

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Topological Insulator'
+copyright = '2021, Chen Ze'
+author = 'Chen Ze'
 
 release = '0.1'
 version = '0.1.0'
